49641f67cdf5e3ca8a99d84cff8a9bb8fbec696d
Mkas <maksikooo@users.noreply.github.com>
2019-06-18 12:17:04 +0300

    4.1 task edited

--- test/4.1 task/src/com/second_task/Main.java
+++ test/4.1 task/src/com/second_task/Main.java
@@ -3,8 +3,8 @@ package com.second_task;
 public class Main {
 
     public static void main(String[] args) {
-        int[] a =null;
-	    int max = 0;
+        int[] a;
+	    int max ;
         a= new initializeArray().main();
 	    max = new max().main(a);
 	    System.out.println(max) ;
